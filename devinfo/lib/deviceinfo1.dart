170b6f331e3b7e3dd62e187f4d23c51515f86da1
Sairam Sah <sairam2054@gmail.com>
2024-02-07 15:27:45 +0545

    init

--- devinfo/lib/deviceinfo1.dart
+++ devinfo/lib/deviceinfo1.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
-
-
-class DeviceInfo1 extends StatefulWidget {
-  const DeviceInfo1(String s, {super.key});
-
-  @override
-  State<DeviceInfo1> createState() => _DeviceInfo1State();
-}
-
-class _DeviceInfo1State extends State<DeviceInfo1> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+class DeviceInfo extends StatelessWidget {
+    final String deviceInfo;
+    const DeviceInfo({Key? key,required this.deviceInfo, required deviceInfoPlugin}) : super(key:key);
+  
+    @override
+    Widget build(BuildContext context) {
+      return  Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:Text(
+          deviceInfo ?? 'Loading device info..' ,
+          maxLines: 10,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(fontSize: 16),
+        )
+      );
+    }
 }
--- devinfo/lib/main.dart
+++ devinfo/lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:devinfo/deviceinfo1.dart';
-import 'package:devinfo/deviceinfo2.dart';
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'deviceinfo1.dart';
+
+void main()=>runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -29,28 +33,123 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static final DeviceInfoPlugin deviceInfoPlugin =DeviceInfoPlugin();
+  Map<String,dynamic>_deviceData=<String,dynamic>{};
+  // late String androidDeviceInfo;
+
+
   @override
-  Widget build(BuildContext context) {
+  void initState(){
+    initPlatformState();
+    super.initState();
+    // fetchDeviceDetails();
+    }
+
+      FutureOr<void>initPlatformState()async{
+      var deviceData =<String,dynamic>{};
+
+      try{
+        if(Platform.isAndroid){
+        deviceData= _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+        }else if(Platform.isIOS){
+          deviceData= _readIosBuildData(await deviceInfoPlugin.iosInfo);
+        }
+      } on PlatformException{
+        deviceData=<String,dynamic>{'Error':'Faild to get platform version'};
+      }
+
+      if(!mounted)return;
+
+      setState(() {
+        _deviceData=deviceData;
+      });
+
+    }
+
+
+    Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+      'displaySizeInches':
+          ((build.displayMetrics.sizeInches * 10).roundToDouble() / 10),
+      'displayWidthPixels': build.displayMetrics.widthPx,
+      'displayWidthInches': build.displayMetrics.widthInches,
+      'displayHeightPixels': build.displayMetrics.heightPx,
+      'displayHeightInches': build.displayMetrics.heightInches,
+      'displayXDpi': build.displayMetrics.xDpi,
+      'displayYDpi': build.displayMetrics.yDpi,
+      'serialNumber': build.serialNumber,
+    };
+  }
+
+    Map<String, dynamic> _readIosBuildData(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+    
+    @override
+    Widget build(BuildContext context) {
     return DefaultTabController(
         length: 2,
         child: Scaffold(
             appBar: AppBar(
-              title: const Text('Device Info Tabs'),
+              title: const Text('Device Info '),
               centerTitle: true,
               bottom: const TabBar(tabs: [
                 Tab(
-                  text: 'Device 1',
+                  text: 'Android',
                 ),
                 Tab(
-                  text: 'Device 2',
+                  text: 'Internal',
                 ),
               ]),
             ),
-            body: const TabBarView(
-              children: [
-                DeviceInfo1('Device index :1'),
-                DeviceInfo2('Device index :1'),
-              ],
-            )));
+            body:TabBarView(
+              children:[
+                DeviceInfo(deviceInfoPlugin: _deviceData['androidDeviceInfo'], deviceInfo: '',),
+                // DeviceInfo(deviceInfoPlugin: _deviceData['IosDeviceInfo']),
+              ]
+              
+            )
+            ),
+            );
+            
   }
-}
+  }
